4fa8d8b32c26e829ce71a1b0eb6816d72857e446
David Graus <david.graus@fdmediagroep.nl>
2019-01-20 14:29:57 +0100

    move recursion out of main function, but rather in __main__

--- run-obd.py
+++ run-obd.py
@@ -4,11 +4,15 @@ import obd
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
+
 def connect_obd():
+    """
+    Try to establish connection w/ OBD2 with 10 sec re-try intervals
+    """
+
     while True:
         conn = obd.OBD(portstr="/dev/rfcomm0", baudrate=None, fast=False)
         if conn.is_connected():
-            obd.logger.debug("Connected! Returning {}".format(conn))
             return conn
         else:
             obd.logger.debug("Unable to connect, trying again in 10...")
@@ -23,6 +27,7 @@ def force_push_commands(conn, output_file):
     """
     Force push all commands (in mode 1).
     """
+
     with open(output_file, 'a') as out_file:  # TODO: one-time force query to check supported commands
         for c in obd.commands[1]:
             r = conn.query(c, force=True)
@@ -43,24 +48,20 @@ def main(conn, commands):
 
         while conn.is_connected():
             data = [conn.query(c) for c in commands]
-            data = [get_timestamp()] + [c.value for c in data]
+            data = [get_timestamp()] + [c.value for c in data]  # TODO: write raw values (magnitude?)
 
             csv_file.write("\t".join(map(str, data)) + "\n")
             csv_file.flush()
 
-        obd.logger.debug("Disconnected(?) Reconnecting + restarting main...")
-
-        conn = connect_obd()
-        main(conn, commands)
-
+        obd.logger("Lost connection?")
 
 
 if __name__ == "__main__":
-    conn = connect_obd()
-
     cmds = {b'0103', b'0104', b'0105', b'0106', b'0107', b'010B', b'010C', b'010D', b'010E', b'010F', b'0111', b'0113',
             b'0114', b'0115'}  # extracted from logs
     cmds = [c for c in obd.commands[1] if c.command in cmds]
 
-    force_push_commands(conn, '/home/graus/obd-all-commands.tsv')
-    main(conn, cmds)
+    while True:
+        conn = connect_obd()
+        force_push_commands(conn, '/home/graus/obd-all-commands.tsv')  # one-off function to check commands
+        main(conn, cmds)
